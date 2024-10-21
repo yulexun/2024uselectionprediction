3b4a41ca2b7dde18ff05c3f5479658f875ba3294
Yu, Lexun <lyu@prpr.one>
2024-10-20 20:40:17 -0400

    filter data by national or state

--- scripts/03-clean_data.R
+++ scripts/03-clean_data.R
@@ -1,5 +1,5 @@
 #### Preamble ####
-# Purpose: Cleans the raw plane data recorded by two observers..... [...UPDATE THIS...]
+# Purpose: Cleans the raw  data from FiveThirtyEight
 # Author: Rohan Alexander [...UPDATE THIS...]
 # Date: 6 April 2023 [...UPDATE THIS...]
 # Contact: rohan.alexander@utoronto.ca [...UPDATE THIS...]
@@ -13,27 +13,40 @@ library(tidyverse)
 #### Clean data ####
 raw_data <- read_csv("data/01-raw_data/president_polls.csv")
 
-# Delete NA columns, 
-# Note: I am not sure if I deleting state here is appropriate, the half of the state column are missing.
+
 cleaned_data <- raw_data |>
-  select(-sponsor_ids, 
-    -sponsors, 
-    -sponsor_candidate_id, 
-    -sponsor_candidate, 
-    -sponsor_candidate_party, 
-    -state, 
-    -endorsed_candidate_id, 
-    -endorsed_candidate_name, 
-    -endorsed_candidate_party, 
-    -subpopulation, -tracking, 
-    -notes, -url_article, -url_topline, -url_crosstab,
-    -source, -internal, -partisan, -seat_name, 
-    -ranked_choice_round) |> na.omit()
+  filter(
+    numeric_grade >= 2.7
+  )
+
+cleaned_data_national <- cleaned_data |>
+  filter(is.na(state))
+
+cleaned_data_state <- cleaned_data |>
+  filter(!is.na(state))
+
+# Delete NA columns
+# cleaned_data <- raw_data |>
+#   select(-sponsor_ids, 
+#     -sponsors, 
+#     -sponsor_candidate_id, 
+#     -sponsor_candidate, 
+#     -sponsor_candidate_party, 
+#     -endorsed_candidate_id, 
+#     -endorsed_candidate_name, 
+#     -endorsed_candidate_party, 
+#     -subpopulation, -tracking, 
+#     -notes, -url_article, -url_topline, -url_crosstab,
+#     -source, -internal, -partisan, -seat_name, 
+#     -ranked_choice_round) |> na.omit()
+
 
 # Keep data from one pollster for Appendix
-morningconsult_data <- cleaned_data %>%
-  filter(pollster == "Morning Consult")
+# morningconsult_data <- cleaned_data %>%
+#   filter(pollster == "Morning Consult")
 
 #### Save data ####
 write_csv(cleaned_data, "data/02-analysis_data/cleaned_data.csv")
-write_csv(morningconsult_data, "data/02-analysis_data/mc_data.csv")
+write_csv(cleaned_data_national, "data/02-analysis_data/cleaned_data_national.csv")
+write_csv(cleaned_data_national, "data/02-analysis_data/cleaned_data_state.csv")
+# write_csv(morningconsult_data, "data/02-analysis_data/mc_data.csv")
