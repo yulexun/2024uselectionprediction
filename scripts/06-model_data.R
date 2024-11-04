2da635b800f2aee966a49d791b7ba18ffadabac9
Siddharth Gowda <siddharth@skube.xyz>
2024-11-03 22:44:44 -0500

    changed model script to create model for trump and harris

--- scripts/06-model_data.R
+++ scripts/06-model_data.R
@@ -34,6 +34,8 @@ library(here)
 #   file = "models/first_model.rds"
 # )
 
+set.seed(123)
+
 cleaned_data = read_csv(here("data/02-analysis_data/cleaned_data.csv"))
 baye_model_data = cleaned_data[cleaned_data$candidate_name == "Kamala Harris", ]
 # Define the Bayesian model with brms
@@ -56,7 +58,7 @@ bayesian_model_1 <- stan_glmer(
 
 saveRDS(
   bayesian_model_1,
-  file = "models/bayesian_model_1.rds"
+  file = "models/bayesian_model_1_harris.rds"
 )
 
 
@@ -65,8 +67,8 @@ train_indices <- sample(seq_len(nrow(just_harris_data)), size = 0.7 * nrow(just_
 train_data <- just_harris_data[train_indices, ]
 test_data <- just_harris_data[-train_indices, ]
 
-write.csv(train_data, "./data/02-analysis_data/train_data.csv")
-write.csv(test_data, "./data/02-analysis_data/test_data.csv")
+write.csv(train_data, "./data/02-analysis_data/train_data_harris.csv")
+write.csv(test_data, "./data/02-analysis_data/test_data_harris.csv")
 
 # Fit the model on the training data
 bayesian_model_train <- stan_glmer(
@@ -82,5 +84,60 @@ bayesian_model_train <- stan_glmer(
 
 saveRDS(
   bayesian_model_train,
-  file = "models/bayesian_model_train.rds"
+  file = "models/bayesian_model_train_harris.rds"
 )
+
+
+##### Trump Model Save
+
+cleaned_data = read_csv(here("data/02-analysis_data/cleaned_data.csv"))
+baye_model_data = cleaned_data[cleaned_data$candidate_name == "Donald Trump", ]
+# Define the Bayesian model with brms
+# test and train dataset
+
+formula <- pct ~ pollscore + days_taken_from_election + sample_size + (1 | methodology) + (1 | state)
+
+priors = normal(0, 2.5, autoscale = TRUE)
+
+bayesian_model_1 <- stan_glmer(
+  formula = formula,
+  data = baye_model_data,
+  family = gaussian(),
+  prior = priors,
+  prior_intercept = priors,
+  seed = 123,
+  cores = 4,
+  adapt_delta = 0.95
+)
+
+saveRDS(
+  bayesian_model_1,
+  file = "models/bayesian_model_1_trump.rds"
+)
+
+
+just_harris_data = cleaned_data[cleaned_data$candidate_name == "Donald Trump", ]
+train_indices <- sample(seq_len(nrow(just_harris_data)), size = 0.7 * nrow(just_harris_data))
+train_data <- just_harris_data[train_indices, ]
+test_data <- just_harris_data[-train_indices, ]
+
+write.csv(train_data, "./data/02-analysis_data/train_data_trump.csv")
+write.csv(test_data, "./data/02-analysis_data/test_data_trump.csv")
+
+# Fit the model on the training data
+bayesian_model_train <- stan_glmer(
+  formula = formula,
+  data = train_data,
+  family = gaussian(),
+  prior = priors,
+  prior_intercept = priors,
+  seed = 123,
+  cores = 4,
+  adapt_delta = 0.95
+)
+
+saveRDS(
+  bayesian_model_train,
+  file = "models/bayesian_model_train_trump.rds"
+)
+
